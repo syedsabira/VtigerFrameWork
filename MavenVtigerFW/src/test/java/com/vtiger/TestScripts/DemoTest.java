6b477a8d4308e6bdfa0d44b59c749ef30d73df0d
sabira <sabira@sabira-PC>
2020-06-24 15:27:17 +0530

    transferring updated file

---
+++ MavenVtigerFW/src/test/java/com/vtiger/TestScripts/DemoTest.java
@@ -0,0 +1,14 @@
+package com.vtiger.TestScripts;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void checkbox()
+	{
+	  System.out.println("Demo Running");
+	  System.out.println("Demo Test");	
+
+	}
+
+}
